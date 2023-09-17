ddad16dddb443dfbc6cd172d045c882caf6e850a
clarenceta07 <clarenceta2018@gmail.com>
2023-09-17 10:41:27 -0400

    (added video slider, neeed to make transition)

---
+++ script.js
@@ -0,0 +1,35 @@
+//menu toggle
+const menuToggle = document.querySelector('.toggle');
+          const showcase = document.querySelector('.showcase');
+    
+          menuToggle.addEventListener('click', () => {
+            menuToggle.classList.toggle('active');
+            showcase.classList.toggle('active');
+          })
+
+//video slider
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+
+
+var sliderNav = function(manual) {
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+    
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+
+
+}
+
+btns.forEach((btn,i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+});
+
