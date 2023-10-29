48ce04ab5c5b3b2b699f60015762f8eed9322905
clarenceta07 <clarenceta2018@gmail.com>
2023-10-29 10:58:00 -0400

    (checkout page)

--- cart.js
+++ cart.js
@@ -1,39 +1,3 @@
-//menu toggle
-const menuToggle = document.querySelector('.toggle');
-          const showcase = document.querySelector('.showcase');
-    
-          menuToggle.addEventListener('click', () => {
-            menuToggle.classList.toggle('active');
-            showcase.classList.toggle('active');
-
-          })
-
-//disable scrolling when navbar is opened
-const scrollToggleButton = document.getElementById("toggleButton");
-
-scrollToggleButton.addEventListener("click", function () {
-    const body = document.body;
-    if (body.style.overflow === "hidden") {
-        body.style.overflow = "auto"; // Enable scrolling
-    } else {
-        body.style.overflow = "hidden"; // Disable scrolling
-    }
-});
-
-//video slider
-document.addEventListener("DOMContentLoaded", function () {
-    const carousel = new Flickity(".carousel", {
-      cellAlign: "left",
-      contain: true,
-      wrapAround: true,
-      draggable: true,
-      autoPlay: 5000,
-      prevNextButtons: false,
-      pageDots: true,
-      pageDotPosition: "center"
-    });
-});
-
 // Initialize the cart items array and total
 const cart = JSON.parse(localStorage.getItem('cart')) || [];
 let total = 0;
@@ -42,9 +6,13 @@ let total = 0;
 function displayCart() {
     const cartList = document.getElementById('cart-items');
     const cartTotal = document.getElementById('cart-total');
+    const totalQuantity = document.getElementById('total-quantity'); // New line
+
     cartList.innerHTML = ''; // Clear the existing cart items
 
+
     total = 0;
+    quantity = 0; // New line
     cart.forEach(item => {
         // Create a "Remove" button for the item
         const removeButton = document.createElement('button');
@@ -58,9 +26,13 @@ function displayCart() {
             removeItemFromCart(itemToRemove);
         });
 
-        // Create a list item for the cart
-        const cartItem = document.createElement('li');
-        cartItem.innerHTML = `${item.name}: $${item.price}`;
+ // Create a list item for the cart
+ const cartItem = document.createElement('li');
+ cartItem.innerHTML = `
+     <img src="${item.image}" alt="${item.name}" class="cart-item-image">
+     ${item.name}: $${item.price}
+ `;
+        
         cartItem.appendChild(removeButton);
 
         // Add the item to the cart display
@@ -68,15 +40,17 @@ function displayCart() {
 
         // Update the total
         total += item.price;
+        quantity++; // Increment the quantity
     });
 
     cartTotal.textContent = `$${total.toFixed(2)}`;
+    totalQuantity.textContent = quantity; // Update the total quantity
 }
 
 // Function to remove an item from the cart
-function removeItemFromCart(item) {
+function removeItemFromCart(itemToRemove) {
     // Find the index of the item to remove
-    const itemIndex = cart.indexOf(item);
+    const itemIndex = cart.indexOf(itemToRemove);
 
     if (itemIndex !== -1) {
         // Remove the item from the cart
@@ -98,26 +72,17 @@ addToCartButtons.forEach(button => {
         const foodItem = button.parentElement.parentElement;
         const itemName = foodItem.querySelector('h5').textContent;
         const itemPrice = parseFloat(button.getAttribute('data-price')); // Get the item price from the data attribute
-        const itemImage = foodItem.querySelector('img').src;
 
         // Add the item to the cart
-        cart.push({ name: itemName, price: itemPrice, image: itemImage });
-
-        // Update local storage
-        localStorage.setItem('cart', JSON.stringify(cart));
+        cart.push({ name: itemName, price: itemPrice });
 
         // Update the cart display
         displayCart();
 
-       
+        // Update local storage
+        localStorage.setItem('cart', JSON.stringify(cart));
     });
 });
 
 // Display the cart when the page loads
 displayCart();
-
-
-
-
-
-
