639a4a8e434d57cce70be06e799c803fbf0e42e4
clarenceta07 <clarenceta2018@gmail.com>
2023-11-18 12:09:36 -0500

    New message for the combined commit

--- js/script.js
+++ js/script.js
@@ -263,17 +263,7 @@ window.addEventListener("scroll", function() {
     }
 });
 
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement(
-        { pageLanguage: 'en' },
-        'google_translate_element'
-    );
-}
 
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-document.getElementsByTagName('head')[0].appendChild(script);
 
 
 
